afc019560b409af9fcf3b668cb496286872c5dab
Jimboskii <jimmyngpham@gmail.com>
2021-06-08 10:45:20 -0500

    Update NurseEntriesController.java

--- src/main/java/com/example/controller/NurseEntriesController.java
+++ src/main/java/com/example/controller/NurseEntriesController.java
@@ -1,40 +1,40 @@
-package com.example.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.models.NurseEntries;
-import com.example.repositorys.NurseEntriesRepository;
-import com.example.service.HospitalService;
-
-import javassist.NotFoundException;
-
-public class NurseEntriesController {
-
-	@Autowired
-	NurseEntriesRepository ner;
-	
-	@Autowired
-	HospitalService hs;
-	
-	@GetMapping
-	public String findREntries(@RequestParam(name = "id") NurseEntries nurseEntries, Model model)
-	{
-		List<NurseEntries> ne = null;
-		try {
-			ne = hs.findAllNurseEntries();
-		}
-		catch (NotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		model.addAttribute("id", ne);
-		return "id";
-		
-	}
-	
-}
+//package com.example.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//import com.example.models.NurseEntries;
+//import com.example.repositorys.NurseEntriesRepository;
+//import com.example.service.HospitalService;
+//
+//import javassist.NotFoundException;
+//
+//public class NurseEntriesController {
+//
+//	@Autowired
+//	NurseEntriesRepository ner;
+//	
+//	@Autowired
+//	HospitalService hs;
+//	
+//	@GetMapping
+//	public String findREntries(@RequestParam(name = "id") NurseEntries nurseEntries, Model model)
+//	{
+//		List<NurseEntries> ne = null;
+//		try {
+//			ne = hs.findAllNurseEntries();
+//		}
+//		catch (NotFoundException e)
+//		{
+//			e.printStackTrace();
+//		}
+//		model.addAttribute("id", ne);
+//		return "id";
+//		
+//	}
+//	
+//}
